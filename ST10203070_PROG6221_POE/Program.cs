d4fbab0d5ab39843d956bb1ffdabeb22b5169588
MaxGWalsh <st10203070@vcconnect.edu.za>
2023-04-16 14:30:06 +0200

    Fifth commit:
    Program class - added main method call to switch statement for when a user decides to clear recipe. This is so they can enter a new recipe. Changed foreground colour of goodbye message to blue so it matches the welcome message. Included break statement inside if statement to exit the while loop if user decides to clear and enter a new recipe so that the option menu is not repeated when the user exits the application.

--- ST10203070_PROG6221_POE/Program.cs
+++ ST10203070_PROG6221_POE/Program.cs
@@ -14,20 +14,20 @@ namespace ST10203070_PROG6221_POE
         public int numSteps = 0;
 
         //Program class constructor
-        public Program() 
+        public Program()
         {
-            
+
         }
 
         //Main method
-        static void Main(String[] args) 
+        static void Main(String[] args)
         {
             //Creating object of Recipe class
             Recipe recipe = new Recipe();
             //Creating object instance of Program
             var prog = new Program();
 
-            //Setting foreground colour to blue
+            //Setting foreground colour to blue for welcome message
             Console.ForegroundColor = ConsoleColor.Blue;
             //Welcome message
             Console.WriteLine("Welcome to recipe manager!", Console.ForegroundColor);
@@ -42,7 +42,7 @@ namespace ST10203070_PROG6221_POE
             prog.numIngredients = Convert.ToInt32(Console.ReadLine());
 
             //Loop to get and store details for each ingredient: Name, Quantity, Unit of measurement
-            for (int i = 0; i < prog.numIngredients; i++) 
+            for (int i = 0; i < prog.numIngredients; i++)
             {
                 //Asking for details of specific ingredient number
                 Console.WriteLine("Enter details for ingredient " + (i +1));
@@ -53,7 +53,7 @@ namespace ST10203070_PROG6221_POE
                 //Asking for ingredient quantity
                 Console.WriteLine("Quantity: ");
                 //Saving quantity in varibale quantity
-                double quantity = Convert.ToDouble(Console.ReadLine()); 
+                double quantity = Convert.ToDouble(Console.ReadLine());
                 //Asking for unit of measurement
                 Console.WriteLine("Unit of measurement: ");
                 //Saving unit of measurement in varibale unit
@@ -68,15 +68,15 @@ namespace ST10203070_PROG6221_POE
             //Getting number of steps
             Console.WriteLine("Enter the number of steps: ");
             //Saving number of steps in varibale numSteps
-            prog.numSteps = Convert.ToInt32(Console.ReadLine()); 
+            prog.numSteps = Convert.ToInt32(Console.ReadLine());
 
             //Getting and saving description for each step to steps array based on numSteps the user wants to enter
-            for(int i = 0; i < prog.numSteps; i++) 
+            for (int i = 0; i < prog.numSteps; i++)
             {
                 //Getting description for steps sequentially
                 Console.WriteLine("Enter description for step " + (i+1) + ": ");
                 //Saving description to variable description
-                string description  = Console.ReadLine();   
+                string description = Console.ReadLine();
                 //Adding description to steps array
                 recipe.AddStep(description);
             }
@@ -91,7 +91,7 @@ namespace ST10203070_PROG6221_POE
             bool run = true;
 
             //While loop giving user furhter options such as scaling recipe, resetting quantities, clearing recipe, or exiting
-            while (run) 
+            while (run)
             {
                 Console.WriteLine("\nPlease select one of the following:");
                 Console.WriteLine("1. Scale recipe");
@@ -99,10 +99,10 @@ namespace ST10203070_PROG6221_POE
                 Console.WriteLine("3. Clear recipe");
                 Console.WriteLine("4. Exit");
                 Console.WriteLine("Enter choice: ");
-                int choice = Convert.ToInt32(Console.ReadLine());   
+                int choice = Convert.ToInt32(Console.ReadLine());
 
                 //Switch statement used to act on users choice
-                switch(choice) 
+                switch (choice)
                 {
                     //Scale recipe
                     case 1:
@@ -127,18 +127,29 @@ namespace ST10203070_PROG6221_POE
                         //Calling ClearRecipe method
                         recipe.ClearRecipe();
                         //Statement confriming recipe cleared
-                        Console.WriteLine("\nRecipe cleared successfully");
+                        Console.WriteLine("\nRecipe cleared successfully\n");
+                        //Calling main method to enter new recipe
+                        Program.Main(args);
                         break;
                     //Exit application
                     case 4:
                         //Setting run varible to false to exit while loop
                         run = false;
+                        //Setting foreground colour to blue for goodbye message
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine("\nGoodbye!");
+                        //Setting foreground colour back to gray
+                        Console.ForegroundColor = ConsoleColor.Gray;
                         break;
                     //Default case for if the user enters an invalid choice
                     default:
                         Console.WriteLine("\nInvalid choice. Please try again.");
                         break;
+                }
+                //If statement to break from loop if user decides to clear and enter new recipe
+                if (choice == 3) 
+                {
+                    break;
                 }
             }
         }
